a4588032bfe5f95fff4bc10a1b06d37b78c1d05a
olpanf <113999331+olpanf@users.noreply.github.com>
2022-12-20 23:12:11 +0300

    qwedd

---
+++ Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    struct Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public char ch { get; set; }
+
+        public static implicit operator Point((int, int, char) value) =>
+              new Point { x = value.Item1, y = value.Item2, ch = value.Item3 };
+
+        public static bool operator ==(Point a, Point b) =>
+                (a.x == b.x && a.y == b.y) ? true : false;
+        public static bool operator !=(Point a, Point b) =>
+                (a.x != b.x || a.y != b.y) ? true : false;
+
+        public void Draw()
+        {
+            DrawPoint(ch);
+        }
+        public void Clear()
+        {
+            DrawPoint(' ');
+        }
+
+        private void DrawPoint(char _ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(_ch);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnakeGame
+{
+   
+   
+   
+
+    enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+
+    
+   
+}
